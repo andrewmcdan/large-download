8f082308c9643b8f505f23cce240b885be35f096
Andrew <andrewmcdan@gmail.com>
2024-04-01 18:08:04 -0400

    update

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,22 @@
-export interface HTTPOptions {
-    retries?: number;
-}
+declare module "large-download" {
+    export interface HTTPOptions {
+        retries?: number;
+        // Include other 'got' package options as needed
+    }
 
-export interface LargeDownloadOptions {
-    link: string;
-    destination: string;
-    timeout?: number;
-    retries?: number;
-    httpOptions?: HTTPOptions;
-    onRetry?: (error: Error) => void;
-    onData?: (downloadedSize: number, declaredSize: string) => void;
-    minSizeToShowProgress?: number;
-}
+    export interface LargeDownloadOptions {
+        link: string;
+        destination: string;
+        timeout?: number;
+        retries?: number;
+        httpOptions?: HTTPOptions;
+        onRetry?: (error: Error) => void;
+        onData?: (downloadedSize: number, declaredSize: string) => void;
+        minSizeToShowProgress?: number;
+    }
 
-export class LargeDownload {
-    constructor(opts: LargeDownloadOptions);
-    load(): Promise<void>;
-}
-
-declare module 'large-download' {
-    export { LargeDownload };
+    export class LargeDownload {
+        constructor(opts: LargeDownloadOptions);
+        load(): Promise<void>;
+    }
 }
