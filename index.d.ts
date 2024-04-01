4f3a1c9f104c2c4a149679ed77400283b31747e6
Andrew <andrewmcdan@gmail.com>
2024-04-01 17:38:55 -0400

    Fix for index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,20 @@
-declare module "large-download" {
-    export interface HTTPOptions {
-        retries?: number;
-        // Include other 'got' package options as needed
-    }
+export interface HTTPOptions {
+    retries?: number;
+    // Additional properties from 'got' can be added here as needed.
+}
 
-    export interface LargeDownloadOptions {
-        link: string;
-        destination: string;
-        timeout?: number;
-        retries?: number;
-        httpOptions?: HTTPOptions;
-        onRetry?: (error: Error) => void;
-        onData?: (downloadedSize: number, declaredSize: string) => void;
-        minSizeToShowProgress?: number;
-    }
+export interface LargeDownloadOptions {
+    link: string;
+    destination: string;
+    timeout?: number;
+    retries?: number;
+    httpOptions?: HTTPOptions;
+    onRetry?: (error: Error) => void;
+    onData?: (downloadedSize: number, declaredSize: string) => void;
+    minSizeToShowProgress?: number;
+}
 
-    export class LargeDownload {
-        constructor(opts: LargeDownloadOptions);
-        load(): Promise<void>;
-    }
+export class LargeDownload {
+    constructor(opts: LargeDownloadOptions);
+    load(): Promise<void>;
 }
